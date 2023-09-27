218fa603c65dafa54e68bcae920a2845a24a25e8
YamiAltena <raka.pradika@ymail.com>
2023-09-27 16:32:34 +0700

    commit selanjutnya

--- Test_Menu.py
+++ Test_Menu.py
@@ -10,45 +10,45 @@ options.add_experimental_option('detach', True)
 options.add_argument('--disable-notifications')
 
 
-
-Menu_Admin_XPATH = '//a[@href="/web/index.php/admin/viewAdminModule"]'
-Menu_PIM_XPATH = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span'
-Menu_MYinfo_XPATH = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span'
-Menu_Maintenance_XPATH = '//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span'
-
 class Menu(unittest.TestCase):
+
+
         
     def setUp(self):
         self.driver = webdriver.Chrome(options = options)
-    
+        
     
     def test_Menu_Admin(self):
         log.test_login(self.driver)
-        self.driver.find_element(By.XPATH,Menu_Admin_XPATH).click()
+        log.Menu_Admin(self.driver)
         time.sleep(3)
         management = self.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6[2]').text
         self.assertIn('User Management', management)
+        print("Successfully go to Menu Admin")
 
     def test_Menu_PIM(self):
         log.test_login(self.driver)
-        self.driver.find_element(By.XPATH,Menu_PIM_XPATH).click()
+        log.Menu_PIM(self.driver)
         time.sleep(3)
-        PIM = self.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6').text
-        self.assertIn('PIM', PIM)
+        Information = self.driver.find_element(By.XPATH,'/html/body/div/div[1]/div[2]/div[2]/div/div[1]/div[1]/div[1]/h5').text
+        self.assertIn('Employee Information', Information)
+        print("Successfully go to Menu PIM")
 
     def test_Menu_MyInfo(self):
         log.test_login(self.driver)
-        self.driver.find_element(By.XPATH,Menu_MYinfo_XPATH).click()
+        log.Menu_Myinfo(self.driver)
         time.sleep(3)
-        PIM = self.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6').text
-        self.assertIn('PIM', PIM)
+        Personal = self.driver.find_element(By.XPATH,'/html/body/div/div[1]/div[2]/div[2]/div/div/div/div[2]/div[1]/h6').text
+        self.assertIn('Personal Details', Personal)
+        print("Successfully go to Menu My Info")
 
-    def test_Menu_Maintenance(self):
+    def test_Menu_Claim(self):
         log.test_login(self.driver)
-        self.driver.find_element(By.XPATH,Menu_Maintenance_XPATH).click()
+        log.Menu_Claim(self.driver)
         time.sleep(3)
-        PIM = self.driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/header/div[1]/div[1]/span/h6').text
-        self.assertIn('PIM', PIM)
+        Claim = self.driver.find_element(By.XPATH,'/html/body/div/div[1]/div[1]/header/div[1]/div[1]/span/h6').text
+        self.assertIn('Claim', Claim)
+        print("Successfully go to Menu Claim")
 
 
     def tearDown(self):
--- log.py
+++ log.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 import time
 
 
+
 def test_user(driver):
     driver.find_element(By.NAME,'username').send_keys('Admin')
     driver.find_element(By.NAME,'password').send_keys('admin123' + Keys.RETURN)
@@ -13,5 +14,19 @@ def test_login(driver):
     driver.implicitly_wait(5)
     test_user(driver)
     time.sleep(3)
+
+def Menu_Admin(driver):
+    driver.find_element(By.XPATH,'//a[@href="/web/index.php/admin/viewAdminModule"]').click()
+
+def Menu_PIM(driver):
+    driver.find_element(By.XPATH,'//*[@id="app"]/div[1]/div[1]/aside/nav/div[2]/ul/li[2]/a/span').click()
+
+def Menu_Myinfo(driver):
+    driver.find_element(By.XPATH,'/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[6]/a/span').click()
+
+def Menu_Claim(driver):
+    driver.find_element(By.XPATH,'/html/body/div/div[1]/div[1]/aside/nav/div[2]/ul/li[11]/a').click()
+
+
 
 
