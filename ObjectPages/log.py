f566575f2f505c0bda461c8c70c63afcca4ee44e
YamiAltena <raka.pradika@ymail.com>
2023-10-16 10:44:34 +0700

    commit selanjutnya

--- ObjectPages/log.py
+++ ObjectPages/log.py
@@ -2,17 +2,26 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-    
+
+def URL(driver):
+    driver.maximize_window()
+    driver.get('https://opensource-demo.orangehrmlive.com/')
+    driver.implicitly_wait(5)
+   
 def test_user(driver):
     driver.find_element(By.NAME,'username').send_keys('Admin')
     driver.find_element(By.NAME,'password').send_keys('admin123' + Keys.RETURN)
 
-def test_login(driver):
-    driver.maximize_window()
-    driver.get('https://opensource-demo.orangehrmlive.com/')
-    driver.implicitly_wait(5)
-    test_user(driver)
-    time.sleep(3)
+def test_user_WrongPass(driver):
+    driver.find_element(By.NAME,'username').send_keys('Admin')
+    driver.find_element(By.NAME,'password').send_keys('admin356' + Keys.RETURN)
+    
+def logout(driver):
+    driver.find_element(By.CLASS_NAME,"oxd-userdropdown").click()
+    driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a").click()
+
+
+
 
 
 
--- Test_login.py
+++ Test_login.py
@@ -15,44 +15,31 @@ class Login(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome(options = options)
-        self.url = 'https://opensource-demo.orangehrmlive.com/'
 
     def test_login(self):
-        self.driver.maximize_window()
-        self.driver.get(self.url)
+        log.URL(self.driver)
         self.driver.implicitly_wait(5)
         log.test_user(self.driver)
         time.sleep(3)
         assert 'OrangeHRM' in self.driver.title
         
     def test_login_FAILED_WrongPassword(self):
-        self.driver.maximize_window()
-        self.driver.get(self.url)
+        log.URL(self.driver)
         self.driver.implicitly_wait(5)
-        self.driver.find_element(By.NAME,'username').send_keys('Admin')
-        self.driver.find_element(By.NAME,'password').send_keys('admin356' + Keys.RETURN)
+        log.test_user_WrongPass(self.driver)
         time.sleep(3)
         invalid = self.driver.find_element(By.ID,'app').text
         self.assertIn('Invalid credentials', invalid)
     
     def test_logout(self):
-        self.driver.maximize_window()
-        self.driver.get(self.url)
+        log.URL(self.driver)
         self.driver.implicitly_wait(5)
-        self.driver.find_element(By.NAME,'username').send_keys('Admin')
-        self.driver.find_element(By.NAME,'password').send_keys('admin123' + Keys.RETURN)
+        log.test_user(self.driver)
         time.sleep(3)
-        self.driver.find_element(By.CLASS_NAME,"oxd-userdropdown").click()
-        time.sleep(3)
-        self.driver.find_element(By.XPATH,"//*[@id='app']/div[1]/div[1]/header/div[1]/div[2]/ul/li/ul/li[4]/a").click()
+        log.logout(self.driver)
         time.sleep(5)    
         login_button = self.driver.find_element(By.XPATH,'//button[@type="submit"]').text
         self.assertIn('Login', login_button)
-
-
-    def tearDown(self):
-        self.driver.quit()
-
 
     def tearDown(self):
         self.driver.quit()
